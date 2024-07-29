f5c0415e372aaff10994f828a9d810aaea38d822
Param-10 <bheleparamveer@gmail.com>
2024-07-29 16:42:09 +0530

    chore: Update setInterval call in setBusesFirst()

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ async function setBusesFirst(what) {
                 updateBuses.call(this);
             }.bind(this)).fail(failure.bind(this));
     }, 10000);
-    //setInterval.call(this, cleanup.bind(this), 10000);
+    setInterval.call(this, cleanup.bind(this), 10000);
 }
 
 function cleanup() {
