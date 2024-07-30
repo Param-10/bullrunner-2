71edc8342ea72dcf2927f243e4a4ad5a26956ccc
Param-10 <bheleparamveer@gmail.com>
2024-07-30 13:22:21 +0530

    chore: Update excludeMyIDs in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var buses;
 var alerts;
 
 var excludeList = ['cam', 'ccexp', 'connect', 'penn', 'pennexpr']
-var excludeMyIDs = ['41231', '4088', '4063', '4056', '4098']
+var excludeMyIDs = ['-1', '36235', '42408', '42980', '43015', '43017', '45753', '48824']
 var routeItem = "popupItem route";
 var busItem = "popupItem bus"
 var stopItem = "popupItem stop"
